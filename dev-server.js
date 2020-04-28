17678489ba90334cb11b038612ef20f46dce67ad
Nicholas Beach <nicholas.beach@accenture.com>
2020-04-28 11:12:07 -0400

    Added dev server. Made test not fail on compile errors

---
+++ dev-server.js
@@ -0,0 +1,15 @@
+const ParcelProxyServer = require('parcel-proxy-server');
+
+const port = 1234;
+
+const server = new ParcelProxyServer({
+    entryPoint: './src/index.html',
+    proxies: {
+        '/api/**': {
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+        },
+    }
+});
+
+server.listen(port, () => console.log(`Dev server started on http://localhost:${port}`));
