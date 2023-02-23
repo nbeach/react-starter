1fb368b5407d6075926d689b3c823029e3ed0274
Nicholas Beach <nicholascbeach@gmail.com>
2023-02-23 10:32:14 -0500

    Switched from SASS to styled components

--- src/component/presentation/Greeting.tsx
+++ src/component/presentation/Greeting.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components";
 
 export type GreetingStateProps = {
     readonly name: string
@@ -9,8 +10,14 @@ export type GreetingDispatchProps = {
 };
 
 export const Greeting = (props: GreetingStateProps & GreetingDispatchProps) => <div data-testid={GreetingTestId.Container}>
-    <div onClick={() => props.nameReset()} data-testid={GreetingTestId.Message}>Hello {props.name}</div>
+    <FancyHeader onClick={() => props.nameReset()} data-testid={GreetingTestId.Message}>Hello {props.name}</FancyHeader>
 </div>
+
+
+const FancyHeader = styled.h1({
+    color: "green",
+    fontFamily: "sans-serif",
+})
 
 export enum GreetingTestId {
     Container = "GREETING_CONTAINER",
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import "./style/index.scss"
 import {initializeStore} from "./store/store"
 import {Provider} from "react-redux"
 import {App} from "./component/presentation/App"
