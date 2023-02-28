5bbdb5ac6afa166aba9b6c19c94bc9bd37081d46
Nicholas Beach <nicholascbeach@gmail.com>
2023-02-27 19:37:42 -0500

    Updated base data

--- src/service/toDoItemService.ts
+++ src/service/toDoItemService.ts
@@ -2,6 +2,8 @@ import {ToDoItem} from "../model/domain";
 
 export const retrieveToDoItems = (): Promise<ReadonlyArray<ToDoItem>> => {
     return Promise.resolve( [
-        { id: "123", text: "Get groceries", highPriority: true }
+        { id: "1", text: "Get groceries", highPriority: true },
+        { id: "2", text: "Go to gym", highPriority: true },
+        { id: "3", text: "Play video games", highPriority: false }
     ])
 }
